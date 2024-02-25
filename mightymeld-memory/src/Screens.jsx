41f2bcbb9361880d6c06502fd2b427506d869dd6
Anony <Anonymakon@gmail.com>
2024-02-25 14:24:12 -0800

    firt commit

---
+++ mightymeld-memory/src/Screens.jsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+import confetti from "canvas-confetti";
+import * as icons from "react-icons/gi";
+import { Tile } from "./Tile";
+
+export const possibleTileContents = [
+  icons.GiHearts,
+  icons.GiWaterDrop,
+  icons.GiDiceSixFacesFive,
+  icons.GiUmbrella,
+  icons.GiCube,
+  icons.GiBeachBall,
+  icons.GiDragonfly,
+  icons.GiHummingbird,
+  icons.GiFlowerEmblem,
+  icons.GiOpenBook,
+];
+
+export function StartScreen({ start }) {
+  return (
+    <div className="flex items-center justify-center pt-12">
+      <div className="flex flex-col items-center bg-pink-50 rounded-md pt-12 px-12 pb-24 gap-8">
+        <h1 className="text-pink-500 font-bold text-3xl">Memory</h1>
+        <p className="text-pink-500 font-medium pb-4">
+          Flip over tiles looking for pairs
+        </p>
+        <button
+          onClick={start}
+          className="bg-gradient-to-t from-pink-400 via-pink-500 to-pink-500 text-white py-2 px-12 rounded-full font-bold"
+        >
+          Play
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export function PlayScreen({ end }) {
+  const [tiles, setTiles] = useState(null);
+  const [tryCount, setTryCount] = useState(0);
+
+  const getTiles = (tileCount) => {
+    // Throw error if count is not even.
+    if (tileCount % 2 !== 0) {
+      throw new Error("The number of tiles must be even.");
+    }
+
+    // Use the existing list if it exists.
+    if (tiles) return tiles;
+
+    const pairCount = tileCount / 2;
+
+    // Take only the items we need from the list of possibilities.
+    const usedTileContents = possibleTileContents.slice(0, pairCount);
+
+    // Double the array and shuffle it.
+    const shuffledContents = usedTileContents
+      .concat(usedTileContents)
+      .sort(() => Math.random() - 0.5)
+      .map((content) => ({ content, state: "start" }));
+
+    setTiles(shuffledContents);
+    return shuffledContents;
+  };
+
+  const flip = (i) => {
+    // Is the tile already flipped? We don’t allow flipping it back.
+    if (tiles[i].state === "flipped") return;
+
+    // How many tiles are currently flipped?
+    const flippedTiles = tiles.filter((tile) => tile.state === "flipped");
+    const flippedCount = flippedTiles.length;
+
+    // Don't allow more than 2 tiles to be flipped at once.
+    if (flippedCount === 2) return;
+
+    // On the second flip, check if the tiles match.
+    if (flippedCount === 1) {
+      setTryCount((c) => c + 1);
+
+      const alreadyFlippedTile = flippedTiles[0];
+      const justFlippedTile = tiles[i];
+
+      let newState = "start";
+
+      if (alreadyFlippedTile.content === justFlippedTile.content) {
+        confetti({
+          ticks: 100,
+        });
+        newState = "matched";
+      }
+
+      // After a delay, either flip the tiles back or mark them as matched.
+      setTimeout(() => {
+        setTiles((prevTiles) => {
+          const newTiles = prevTiles.map((tile) => ({
+            ...tile,
+            state: tile.state === "flipped" ? newState : tile.state,
+          }));
+
+          // If all tiles are matched, the game is over.
+          if (newTiles.every((tile) => tile.state === "matched")) {
+            setTimeout(end, 0);
+          }
+
+          return newTiles;
+        });
+      }, 1000);
+    }
+
+    setTiles((prevTiles) => {
+      return prevTiles.map((tile, index) => ({
+        ...tile,
+        state: i === index ? "flipped" : tile.state,
+      }));
+    });
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center pt-12 gap-4">
+        <div className="flex items-center gap-2">
+          <h2 className="text-indigo-500 font-normal text-base">Tries</h2>
+          <div className="bg-indigo-200 px-3 rounded text-indigo-600 text-md font-medium">
+            {tryCount}
+          </div>
+        </div>
+        <div className="grid grid-cols-4 gap-4 bg-indigo-50 p-3 rounded-md">
+          {getTiles(16).map((tile, i) => (
+            <Tile
+              className="grid grid-cols-4 gap-4 bg-indigo-50 p-3 rounded-md"
+              flip={() => flip(i)}
+              {...tile}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
